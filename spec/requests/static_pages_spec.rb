cd22aedd92cbd46a822156b7adcecb3d9442fecc
lanxi <lhuang38@wisc.edu>
2014-02-24 11:53:50 -0600

    stablize routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,3 @@
-
 require 'spec_helper'
 
 describe "StaticPages" do
@@ -6,17 +5,17 @@ describe "StaticPages" do
 describe "Home page" do
 
     it "should have the content 'Project 2'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Project 2')
     end
     
     it "should have the base title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("CS369 Project 2")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
 end
@@ -24,11 +23,11 @@ end
 describe "Faq page" do
 
     it "should have the content 'Faq'" do
-      visit '/static_pages/faq'
+      visit faq_path
       expect(page).to have_content('Faq')
     end
 it "should have the title 'Faq'" do
-      visit '/static_pages/faq'
+      visit faq_path
       expect(page).to have_title("CS369 Project 2 | Faq")
     end
   end
@@ -36,22 +35,22 @@ it "should have the title 'Faq'" do
 describe "About page" do
 
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About')
     end
   it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("CS369 Project 2 | About")
     end
   end
 describe "Blog page" do
 
     it "should have the content 'Blog'" do
-      visit '/static_pages/blog'
+      visit blog_path
       expect(page).to have_content('Blog')
     end
   it "should have the title 'Blog'" do
-      visit '/static_pages/blog'
+      visit blog_path
       expect(page).to have_title("CS369 Project 2 | Blog")
     end
   end
