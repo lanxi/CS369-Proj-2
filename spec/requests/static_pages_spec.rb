26389521d7794b3bdb946e7442a7bb55e016cf8e
lanxi <lhuang38@wisc.edu>
2014-02-24 12:36:13 -0600

    complete rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,57 +1,61 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  
-describe "Home page" do
+describe "Static pages" do
 
-    it "should have the content 'Project 2'" do
-      visit root_path
-      expect(page).to have_content('Project 2')
-    end
-    
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("CS369 Project 2")
-    end
+  subject { page }
 
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
-end
- 
-describe "Faq page" do
-
-    it "should have the content 'Faq'" do
-      visit faq_path
-      expect(page).to have_content('Faq')
-    end
-it "should have the title 'Faq'" do
-      visit faq_path
-      expect(page).to have_title("CS369 Project 2 | Faq")
-    end
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
   end
 
-describe "About page" do
+  describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'About'" do
-      visit about_path
-      expect(page).to have_content('About')
-    end
-  it "should have the title 'About'" do
-      visit about_path
-      expect(page).to have_title("CS369 Project 2 | About")
-    end
+    let(:heading)    { '' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
-describe "Blog page" do
 
-    it "should have the content 'Blog'" do
-      visit blog_path
-      expect(page).to have_content('Blog')
-    end
-  it "should have the title 'Blog'" do
-      visit blog_path
-      expect(page).to have_title("CS369 Project 2 | Blog")
-    end
+  describe "Faq page" do
+    before { visit faq_path }
+
+    let(:heading)    { 'Faq' }
+    let(:page_title) { 'Faq' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "About page" do
+    before { visit about_path }
+
+    let(:heading)    { 'About' }
+    let(:page_title) { 'About' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  describe "Blog page" do
+    before { visit blog_path }
+
+    let(:heading)    { 'Blog' }
+    let(:page_title) { 'Blog' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Faq"
+    expect(page).to have_title(full_title('Faq'))
+    click_link "Blog"
+    expect(page).to have_title(full_title('Blog'))
+    click_link "Home"
+    click_link "Project 2"
+    expect(page).to have_title(full_title(''))
   end
 end
